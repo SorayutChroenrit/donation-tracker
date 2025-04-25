be67ec76ea4124c39290476f70576320e416468c
Sorayut CH. <sorayutapple@gmail.com>
2025-04-25 21:27:59 +0700

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/donation-tracker",
+  base: "/donation-tracker/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
